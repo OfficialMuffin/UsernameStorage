1cd959e838c6851a941381d71d0b89f780b4174b
OfficialMuffin <39170130+OfficialMuffin@users.noreply.github.com>
2021-11-22 22:41:50 +0000

    Satisfy most pylint errors in src/menu.py

--- src/menu.py
+++ src/menu.py
@@ -1,3 +1,7 @@
+"""
+Provides critical program related functions
+"""
+from os import error
 import re
 
 users = []
@@ -14,7 +18,8 @@ menu = [
 ]
 
 def list_users():
-    # Print out users names and the number of users in the list
+    """Prints the number of users currently in the list of users
+    """
     if len(users) == 0:
         print("User list is empty. Please add a user from the menu.")
     else:
@@ -24,17 +29,19 @@ def list_users():
         print("Number of users: " + len_users + "\n")
 
 def add_user():
-    # Add user to the list
+    """Adds a user to the list of users
+    """
     new_user = input("Input User: ")
     # Check if the user already exists
     for user in users:
-    	if new_user in user:
-    		print("Error: User already exists")
+        if new_user in user:
+            print("Error: User already exists")
     users.append(new_user)
     print(f"User {new_user} added successfully")
 
 def delete_user():
-    # Delete user from the list 
+    """Delete a user from the list of users
+    """
     try:
         list_users()
         del_user = input("Enter user you want to delete: ")
@@ -42,17 +49,21 @@ def delete_user():
         print("\nSuccess!\n")
     except ValueError:
         print("\nError: User does not exist!\n")
-        
+
 def change_user():
-    # Change username if user made typo error
+    """Enable user to change a username
+    """
     pass
 
 def sort_users():
-    # Sort list of users in alphabetical order
+    """Sort the user list in alphabetical order
+    """
     users.sort()
     print("User list has been sorted successfully!")
-    
+
 def find_user():
+    """Search for a particular user. Regex can be used here without quotations
+    """
     search = str(input("Enter a user you would like to search for: "))
     if search in users:
         print(f"User: {search}, exists!")
@@ -63,10 +74,12 @@ def find_user():
             regex = re.compile(f"{search_regex}")
             found_user = list(filter(regex.match, users))
             print(found_user)
-        except Exception:
+        except error:
             print("Error: Regex was unable to search.")
 
 def del_all():
+    """Purge all from list! Muhahahaha
+    """
     if len(users) == 0:
         print("User list is empty. Nothing to delete.")
     else:
@@ -76,12 +89,14 @@ def del_all():
             print("\nUser list has been cleared!\n")
         elif confirm == 'n':
             main_menu()
-            
+
 def save_list():
+    """Save the list of users to a file of choice
+    """
     filename = str(input("Please enter a filename to save: "))
     if filename == "":
         print("You didnt enter anything, please try again.")
-    else: 
+    else:
         if len(users) == 0:
             print("User list is empty. Unable to save an empty list")
         else:
@@ -91,8 +106,12 @@ def save_list():
                 file.write(str(user + '\n'))
             file.close()
             print("File Saved Successfully!")
-    
+
 def read_list():
+    """
+        Read a file that contains a list of users
+        and either append to list or use the new list from the file
+    """
     filename = str(input("Enter the filename of file you wish to open: ")) + ".txt"
     try:
         #file = open(filename, 'r')
@@ -108,16 +127,16 @@ def read_list():
                     users.append(file_lines)
                     file.close()
 
-                elif exists == 'n': 
+                elif exists == 'n':
                     # Use the new list in the file
                     file_lines = file.readlines()
-                    users.append(file_lines)             
-        
+                    users.append(file_lines)
     except FileNotFoundError:
         print("File not found. Please try again.")
-    
+
 def main_menu():
-    # Main menu
+    """Main menu of the program
+    """
     print("Please select from the following items: ")
     for menu_number, enumerated_list in enumerate(menu, start=1):
         print(menu_number, enumerated_list)
