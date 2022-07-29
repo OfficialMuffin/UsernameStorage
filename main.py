7dfe69f5eaab22ab455514541879f247cafd36bf
OfficialMuffin <39170130+OfficialMuffin@users.noreply.github.com>
2022-07-29 16:06:55 +0100

    Added funky title

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ by OfficialMuffin
 from time import sleep
 from os import system, name
 from src import menu
+from pyfiglet import Figlet
 
 if __name__ == "__main__":
+    print("Welcome to ")
+    f = Figlet(font='slant')
+    print(f.renderText("UserStore"))
     while True:
         menu.main_menu()
         sleep(3)
